7e567326a8d26a2e674022d9e3902b6fc60a273b
jbt <jbt@NewAIO>
2019-05-23 20:42:52 +0300

    aaa

---
+++ servlets01.intro/src/a/Srv1.java
@@ -0,0 +1,10 @@
+package a;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Srv1")
+public class Srv1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
